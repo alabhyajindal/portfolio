291c91be4373cfc9cb291d94629c84aad5d35024
Alabhya Jindal <alabhya10@gmail.com>
2022-06-08 01:04:46 +0530

    Started creating project details

--- pages/visits.js
+++ pages/visits.js
@@ -4,6 +4,9 @@ export default function visits() {
       <h1 className='text-4xl text-fuchsia-500 font-sans font-semibold select-none tracking-tighter'>
         Visits
       </h1>
+      <p className='text-slate-600 font-medium text-lg tracking-tight'>
+        The main goal of this project was to create a platform ()
+      </p>
     </div>
   );
 }
