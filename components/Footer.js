fc5f0a46cb40559b557f36806e9a35e414b7727f
Alabhya Jindal <alabhya10@gmail.com>
2022-06-08 00:54:43 +0530

    Decreased letter spacing in footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   const date = new Date().getFullYear();
   return (
     <footer className='px-4 py-4 flex justify-between items-center'>
-      <p className='text-slate-500 tracking-tighter'>
+      <p className='text-slate-500 tracking-tight'>
         Alabhya Jindal &copy; {date}
       </p>
       <div className='flex gap-4'>
