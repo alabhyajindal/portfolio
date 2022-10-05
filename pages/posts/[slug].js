7cbbb0989a4a3a885fad49c2035e11efe5ba64b2
Alabhya Jindal <alabhya10@gmail.com>
2022-10-05 23:35:05 +0530

    Created the slug page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,30 @@
-const Post = () => {
-  return <div>what</div>;
+import Head from 'next/head';
+import { getPostFromSlug, getSlugs } from '../../src/api';
+
+const Post = ({ post }) => {
+  return (
+    <div>
+      <Head>
+        <title>{post.meta.title}</title>
+      </Head>
+      <h1 className='text-4xl text-fuchsia-500 font-sans font-semibold select-none tracking-tight'>
+        {post.meta.title}
+      </h1>
+    </div>
+  );
+};
+
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const { content, meta } = getPostFromSlug(slug);
+  // const mdxSource = await serialize(content);
+
+  return { props: { post: { meta } } };
+};
+
+export const getStaticPaths = async () => {
+  const paths = getSlugs().map((slug) => ({ params: { slug } }));
+  return { paths, fallback: false };
 };
 
 export default Post;
--- posts/first.md
+++ posts/first.md
@@ -1,3 +1,11 @@
 ---
 title: How to create water from wine
 ---
+
+## First step
+
+Take water out of fine
+
+## Congratulations
+
+You just completed the tutorial! Now say thanks by donating $1,000.
