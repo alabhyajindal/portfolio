a35e4c98dfcfef7a7f6e8d4c0f1f116dfae51218
Alabhya Jindal <alabhya10@gmail.com>
2022-10-05 23:49:08 +0530

    Parsing and rendering markdown on page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,10 @@
 import Head from 'next/head';
 import { getPostFromSlug, getSlugs } from '../../src/api';
+import { micromark } from 'micromark';
 
 const Post = ({ post }) => {
+  const content = micromark(post.content);
+  console.log(content);
   return (
     <div>
       <Head>
@@ -10,6 +13,7 @@ const Post = ({ post }) => {
       <h1 className='text-4xl text-fuchsia-500 font-sans font-semibold select-none tracking-tight'>
         {post.meta.title}
       </h1>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
 };
@@ -19,7 +23,7 @@ export const getStaticProps = async ({ params }) => {
   const { content, meta } = getPostFromSlug(slug);
   // const mdxSource = await serialize(content);
 
-  return { props: { post: { meta } } };
+  return { props: { post: { content, meta } } };
 };
 
 export const getStaticPaths = async () => {
